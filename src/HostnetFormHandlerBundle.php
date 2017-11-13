35b53524a42b48377bcdaa10cbc409aa18643116
Hidde Boomsma <hboomsma@hostnet.nl>
2017-11-13 11:32:56 +0100

    support autoconfiguration

--- src/HostnetFormHandlerBundle.php
+++ src/HostnetFormHandlerBundle.php
@@ -2,6 +2,8 @@
 namespace Hostnet\Bundle\FormHandlerBundle;
 
 use Hostnet\Bundle\FormHandlerBundle\DependencyInjection\Compiler\FormHandlerRegistryCompilerPass;
+use Hostnet\Component\Form\FormHandlerInterface;
+use Hostnet\Component\FormHandler\HandlerTypeInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -10,6 +12,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 /**
  * @author Yannick de Lange <ydelange@hostnet.nl>
  * @author Iltar van der Berg <ivanderberg@hostnet.nl>
+ * @author Hidde Boomsma <hboomsma@hostnet.nl>
  */
 class HostnetFormHandlerBundle extends Bundle
 {
@@ -24,5 +27,11 @@ class HostnetFormHandlerBundle extends Bundle
 
         parent::build($container);
         $container->addCompilerPass(new FormHandlerRegistryCompilerPass());
+
+        // If auto configuring is available, register tags for the Handler Types.
+        if (method_exists($container, 'registerForAutoconfiguration')) {
+            $container->registerForAutoconfiguration(FormHandlerInterface::class)->addTag('form.handler');
+            $container->registerForAutoconfiguration(HandlerTypeInterface::class)->addTag('form.handler');
+        }
     }
 }
