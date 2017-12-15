5bff004daf580ce941e2c47a413d806b6d52cda9
Gawain Lynch <gawain.lynch@gmail.com>
2017-12-15 12:54:44 +0100

    Add alias for HandlerFactoryInterface

--- test/Functional/ControllerTest.php
+++ test/Functional/ControllerTest.php
@@ -4,9 +4,8 @@
  */
 namespace Hostnet\Bundle\FormHandlerBundle\Functional;
 
-use Hostnet\Bundle\FormHandlerBundle\Functional\Fixtures\TestController;
 use Hostnet\Bundle\FormHandlerBundle\Functional\Fixtures\TestKernel;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpKernel\Kernel;
 
 /**
@@ -14,14 +13,16 @@ use Symfony\Component\HttpKernel\Kernel;
  *
  * @author Gawain Lynch <gawain.lynch@gmail.com>
  */
-class ControllerTest extends KernelTestCase
+class ControllerTest extends WebTestCase
 {
+    private $client;
+
     /**
      * BC for current tests, new tests should get their own config.
      */
     protected function setUp()
     {
-        static::bootKernel(['config_file' => TestKernel::getLegacyConfigFilename()]);
+        $this->client = static::createClient(['config_file' => TestKernel::getLegacyConfigFilename()]);
     }
 
     protected static function createKernel(array $options = array())
@@ -29,7 +30,7 @@ class ControllerTest extends KernelTestCase
         return new TestKernel($options);
     }
 
-    public function test()
+    public function testActionInterfaceDependencyInjection()
     {
         if (Kernel::VERSION_ID < 30300) {
             self::markTestSkipped(sprintf('Symfony version %s not supported by test', Kernel::VERSION));
@@ -39,10 +40,8 @@ class ControllerTest extends KernelTestCase
             $this->markTestSkipped('Sensio Extra bundle is not installed.');
         }
 
-        $container = self::$kernel->getContainer();
-        $container->get(TestController::class);
+        $crawler = $this->client->request('GET', '/');
 
-        // We only care that the controller instantiates correctly
-        $this->addToAssertionCount(1);
+        self::assertSame('test', $crawler->text());
     }
 }
